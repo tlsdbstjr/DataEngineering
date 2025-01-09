9eae28d3a06dfe331ddbd34dcec050a50d066fa1
tlsdbstjr <mx7030@naver.com>
2025-01-09 18:23:50 +0900

    01/08 업데이트
    강의 전 커밋

--- W1/M3/etl_project_gdp.py
+++ W1/M3/etl_project_gdp.py
@@ -5,21 +5,30 @@ import pandas as pd
 import re
 import datetime
 
+LOG_DIR = "./etl_project_log.txt"
+
+# urls
+urlGDP = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+urlRegion = 'https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_the_United_Nations_geoscheme'
+
+# backup file name
+bakupFile = {urlGDP:"wikipediaGDP", urlRegion:"wikipediaRegion"}
+
 # decorator for logging
 # this decorator helps to log the ETL processes.
 # function name, its start and end time, and running time will be logged.
 def withLog(func):
     def wrapper(*args, **kwargs):
-        with open("./etl_project_log.txt", "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%B-%d-%H-%M-%S, "))
-            f.write(f"{func.__name__} start\n")
+        with open(LOG_DIR, "a") as f:
+            f.write(datetime.datetime.now().strftime("%Y-%B-%d-%H-%M-%S,"))
+            f.write(f"{func.__name__},start\n")
             startTime = datetime.datetime.now()
 
             result = func(*args, **kwargs)
 
             endTime = datetime.datetime.now()
-            f.write(endTime.strftime("%Y-%B-%d-%H-%M-%S, "))
-            f.write(f"{func.__name__} end. in {endTime-startTime}\n")
+            f.write(endTime.strftime("%Y-%B-%d-%H-%M-%S,"))
+            f.write(f"{func.__name__},end,{endTime-startTime}{","+args[0] if func.__name__ == "extract" else ""}\n")
         return result
     return wrapper   
 
@@ -27,18 +36,39 @@ def withLog(func):
 # get the gdp data from wikipedia
 @withLog
 def extract(url):
-    # get http response
-    response = requests.get(url)
+    # find past extract log
+    logs = pd.read_csv(LOG_DIR, header=None, names=["time", "function", "status" ,"taken", "url"])
+    lastAccess = logs[logs["url"] == url]
+    
+    # get http responce
+    if lastAccess.empty:
+        response = requests.get(url)
+    else:
+        lastAccess = datetime.datetime.strptime(lastAccess.iloc[-1]['time'],"%Y-%B-%d-%H-%M-%S") - datetime.timedelta(hours=9)
+        response = requests.get(url, headers={"if-Modified-Since":lastAccess.strftime('%a, %d %b %Y %H:%M:%S GMT')})
+    
+    table = ""
 
     # check reponse
     if response.status_code == 200:
         html = response.text
         soup = BeautifulSoup(html, 'html.parser')
+        
+        table = str(soup.select("table.wikitable")[0])
+        
+        # save the responce
+        with open(bakupFile[url]+".bak", "w") as f:
+            f.write(str(table))
+
+    elif response.status_code == 304:
+        # open from backup file
+        try:
+            with open(bakupFile[url]+".bak", "r") as f:
+                table = f.read()
+        except:
+            print(response.status_code)        
     else : 
         print(response.status_code)
-
-    # find all tables
-    table = soup.select("table.wikitable")
 
     return table
 
@@ -56,14 +86,17 @@ def transform(data):
 
 @withLog
 def transformGDPTable(df):
+    # drop column level and rename columns
     df = df.droplevel(level=0, axis=1)
     df.columns = ['Country/Territory', 'IMF Forecast', 'IMF Year', 'World Bank Estimate', 'World BankYear', 'UN Estimate',
        'UN Year']
     cols = df.columns
 
-    for col in cols[2::2]:  # for year columns, transform the data to integer
+    # for year columns, transform the data to integer
+    for col in cols[2::2]: 
         df[col] = df[col].apply(lambda x: int(x) if x.isdigit() else -1)
-    for col in cols[1::2]:  # for GDP data, make billion unit and float
+    # for GDP data, make billion unit and float
+    for col in cols[1::2]:
         df[col] = df[col].apply(lambda x: round(float(x)/1000.0, 2) if x.isdigit() else 0.0)
 
     df = df.drop(index = [0])
@@ -72,15 +105,15 @@ def transformGDPTable(df):
 
 @withLog
 def transformConjungrate(gdp, region):
-    #_gdp = gdp.reset_index(drop=True)
-    #_region = region.reset_index(drop=True)
-
+    # left join with gdp and region.
     res = gdp.merge(
         region[['Country or Area', 'Geographical subregion']],
         left_on='Country/Territory',
         right_on='Country or Area',
         how='left'
     )
+
+    # drop dupplicated column
     res = res.drop('Country or Area', axis=1)
 
     return res
@@ -88,7 +121,7 @@ def transformConjungrate(gdp, region):
 # write the dataFrame to a json file
 @withLog
 def load(dataFrame):
-    # extract dataFrame by json
+    # extract dataFrame to json
     dataFrame.transpose().to_json('Countries_by_GDP.json')
 
 def printOver100B(gdpData):
@@ -101,9 +134,6 @@ def printTop5(gdpData):
     print(top5)
 
 # main
-# urls
-urlGDP = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
-urlRegion = 'https://en.wikipedia.org/wiki/List_of_countries_and_territories_by_the_United_Nations_geoscheme'
 
 # gdp ET process
 gdp = extract(urlGDP)
@@ -117,7 +147,7 @@ region = transform(region)
 # merge two tables
 gdpTable = transformConjungrate(gdp, region)
 
-# and load merged table
+# and L, load merged table
 load(gdpTable)
 
 # print what we want
--- W1/M3/etl_project_gdp_with_sql.py
+++ W1/M3/etl_project_gdp_with_sql.py
@@ -6,6 +6,7 @@ import sqlite3
 
 # data base settings
 database = "World_Economies.db"
+
 # function to send a query
 def sendQuery(sql):
     try:
@@ -18,33 +19,63 @@ def sendQuery(sql):
     except sqlite3.Error as e:
         print(e)
 
+LOG_DIR = "./etl_project_log.txt"
+
+# urls
+url = 'https://www.imf.org/external/datamapper/api/v1/NGDPD'
+
+# backup file name
+bakupFile = {url:"imfGDP"}
+
 # decorator for logging
 # this decorator helps to log the ETL processes.
 # function name, its start and end time, and running time will be logged.
 def withLog(func):
     def wrapper(*args, **kwargs):
-        with open("./etl_project_log.txt", "a") as f:
-            f.write(datetime.datetime.now().strftime("%Y-%B-%d-%H-%M-%S, "))
-            f.write(f"{func.__name__} start\n")
+        with open(LOG_DIR, "a") as f:
+            f.write(datetime.datetime.now().strftime("%Y-%B-%d-%H-%M-%S,"))
+            f.write(f"{func.__name__},start\n")
             startTime = datetime.datetime.now()
 
             result = func(*args, **kwargs)
 
             endTime = datetime.datetime.now()
-            f.write(endTime.strftime("%Y-%B-%d-%H-%M-%S, "))
-            f.write(f"{func.__name__} end. in {endTime-startTime}\n")
+            f.write(endTime.strftime("%Y-%B-%d-%H-%M-%S,"))
+            f.write(f"{func.__name__},end,{endTime-startTime}{","+args[0] if func.__name__ == "extract" else ""}\n")
         return result
     return wrapper   
 
 # get the gdp data from wikipedia
 @withLog
-def extract_IMF_GDP(url):
-    # get http response
-    response = requests.get(url)
+def extract(url):
+    # find past extract log
+    logs = pd.read_csv(LOG_DIR, header=None, names=["time", "function", "status" ,"taken", "url"])
+    lastAccess = logs[logs["url"] == url]
+    
+    # get response
+    if lastAccess.empty:
+        response = requests.get(url)
+    else:
+        lastAccess = datetime.datetime.strptime(lastAccess.iloc[-1]['time'],"%Y-%B-%d-%H-%M-%S") - datetime.timedelta(hours=9)
+        response = requests.get(url, headers={"if-Modified-Since":lastAccess.strftime('%a, %d %b %Y %H:%M:%S GMT')})
+    
+    GDPJson = ""
 
     # check reponse
     if response.status_code == 200:
-        GDPJson= response.text
+        GDPJson = response.text
+        
+        # save the response
+        with open(bakupFile[url]+".bak", "w") as f:
+            f.write(GDPJson)
+
+    elif response.status_code == 304:
+        # read saved response
+        try:
+            with open(bakupFile[url]+".bak", "r") as f:
+                GDPJson = f.read()
+        except:
+            print(response.status_code) 
     else : 
         print(response.status_code)
 
@@ -61,7 +92,7 @@ def transform_IMF_GDP(data):
 
 # write the dataFrame to a json file
 @withLog
-def load(dataFrame:pd.DataFrame, tableName:str, index:bool = False, index_label:str = ""):
+def loadToSQL(dataFrame:pd.DataFrame, tableName:str, index:bool = False, index_label:str = ""):
     # extract dataFrame by jsontry:
     try:
         with sqlite3.connect(database) as conn:
@@ -76,30 +107,33 @@ def load(dataFrame:pd.DataFrame, tableName:str, index:bool = False, index_label:
 
 # import gdp data from imf
 url = 'https://www.imf.org/external/datamapper/api/v1/NGDPD'
-imf_gdp = transform_IMF_GDP(extract_IMF_GDP(url))
-load(imf_gdp, "imf_gdp", index=True, index_label="country")
+imf_gdp = transform_IMF_GDP(extract(url))
+loadToSQL(imf_gdp, "imf_gdp", index=True, index_label="country")
 
 # import iso countries' name data from saved csv file
 ISO_Countries = pd.read_csv('./ISO_3166_Countries.csv')
-load(ISO_Countries, "iso_country_name")
+loadToSQL(ISO_Countries, "iso_country_name")
+
+year = 2024
 
 # print countries whose GDP is over 100B
-sql = """SELECT DISTINCT i.name
-         FROM imf_gdp g
-         JOIN iso_country_name i ON i."alpha-3" = g.country
-         WHERE g."2024" >= 100.0;"""
-print(sendQuery(sql))
+sql = f"""SELECT DISTINCT i.name
+          FROM imf_gdp g
+          JOIN iso_country_name i ON i."alpha-3" = g.country
+          WHERE g."{year}" >= 100.0;"""
+print(*sendQuery(sql), sep="\n")
 
 # print top 5 GDP mean of group by region
-sql ="""WITH ranked_countries AS (
+sql = f"""
+        WITH ranked_countries AS (
             SELECT 
                 g.country,
                 i."sub-region",
-                g."1980" as gdp,
-                RANK() OVER (PARTITION BY i."sub-region" ORDER BY g."1980" DESC) as rank
+                g."{year}" as gdp,
+                RANK() OVER (PARTITION BY i."sub-region" ORDER BY g."{year}" DESC) as rank
             FROM imf_gdp g
             JOIN iso_country_name i ON i."alpha-3" = g.country
-            WHERE g."1980" IS NOT NULL
+            WHERE g."{year}" IS NOT NULL
         )
         SELECT 
             "sub-region",
@@ -108,5 +142,23 @@ sql ="""WITH ranked_countries AS (
         FROM ranked_countries
         WHERE rank <= 5
         GROUP BY "sub-region"
-        ORDER BY avg_gdp DESC;"""
-print(sendQuery(sql))
+        ORDER BY avg_gdp DESC;
+    """
+print(*sendQuery(sql), sep="\n")
+
+sql = f"""
+        WITH ranked_countries AS (
+            SELECT 
+                g.country,
+                i."sub-region",
+                g."{year}" as gdp,
+                RANK() OVER (PARTITION BY i."sub-region" ORDER BY g."{year}" DESC) as rank
+            FROM imf_gdp g
+            JOIN iso_country_name i ON i."alpha-3" = g.country
+            WHERE g."{year}" IS NOT NULL
+        )
+        SELECT *
+        FROM ranked_countries
+        WHERE "sub-region" IS NULL
+    """
+print(*sendQuery(sql), sep="\n")
